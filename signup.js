37d9ea858ba14df8c1604dded0826090cbd6c209
hyunjoo_y <64509600+hyunjoo-y@users.noreply.github.com>
2025-03-17 16:49:59 +0900

    Update signup.js

--- signup.js
+++ signup.js
@@ -97,20 +97,20 @@ async function signup(){
             }
         ]
         // 프라이빗 네트워크 연결
-        var web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.230.154:8545"));
+        var web3 = new Web3(new Web3.providers.HttpProvider("http://blockchain_IP"));
          // 연결 계좌 확인
-         var bal = await web3.eth.getBalance("0x1de4635765c03a76dccf125d60aee010e3ff02c1");
+         var bal = await web3.eth.getBalance("account_input(Ox...)");
          console.log(bal);
          //web3.eth.defaultAccount = web3.eth.accounts[0];
       // 계정 unlock
-         web3.eth.personal.unlockAccount("0x1de4635765c03a76dccf125d60aee010e3ff02c1", "1234")
+         web3.eth.personal.unlockAccount("account_input(0x...)", "password_input")
          .then(console.log('Account unlocked!'));
       // contract 연결
-         var contract = new web3.eth.Contract(ABI,"0x0a50Bce67189cFBA6a9eE924F8aDa42A8703298D");
+         var contract = new web3.eth.Contract(ABI,"contract_address");
          console.log(contract);
-         //var result = await contract.methods.checkUser(id).call({from:'0x1de4635765c03a76dccf125d60aee010e3ff02c1'});
+         //var result = await contract.methods.checkUser(id).call({from:'account_input(0x...)});
       // 회원 가입
-         var result = await contract.methods.register(id,psd).send({from:'0x1de4635765c03a76dccf125d60aee010e3ff02c1'})
+         var result = await contract.methods.register(id,psd).send({from:'account_input(0x...)'})
          .then(function(receipt){});
          console.log(result);
     }catch(error){
